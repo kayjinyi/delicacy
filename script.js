6d305ea7cfd2f682ce2c9776862ee4e543d09cf2
jittel <nathan.c.ginn@gmail.com>
2022-04-11 14:29:02 -0700

    gets cards dynamically generated with no inneer content yet

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const options = {
 // initialized variables
 var recipeButton = document.getElementById("recipe");
 var searchedFood = "";
+var element = "";
+var recipeContainer = document.getElementById("recipe-container");
 
 
 // function that gets recipe data from tasty api
@@ -23,54 +25,79 @@ function getRecipe() {
     fetch(`https://tasty.p.rapidapi.com/recipes/list?from=0&size=5&q=${searchedFood}`, options)
         .then(response => response.json())
         .then(response => {
+            //add start row
+            content += '<div class="row">'
             // loops through results to show names and url's
             for (let i = 0; i < response.results.length; i++) {
-                const element = response.results[i];
+                var content = "";
+                element = response.results[i];
                 // console.log(element.name, element.video_url)
                 console.log(element.name, element.instructions)
 
+                // displayRecipe();
+
+            content += '<div class="col s12 m5">'
+            content += '<div class="card-panel teal">'
+            content += `<span class="white-text">recipe goes here, probably in an ordered list</span>`
+            content += '</div>';
+            content += '</div>';
+            recipeContainer.innerHTML += content;
             }
+            content += '</div>';
         })
         .catch(err => console.error(err));
 }
 
 recipeButton.addEventListener("click", getRecipe);
 
-// fucntion that displays the recipe info
-function displayRecipe() {
-    //stuff here
-}
 
-var FavoriteBtnEl = document.querySelector('#favoritefood');
-var favoriteTerm = document.querySelector('#listGroup');
-console.log("linked")
-const options = {
-	method: 'GET',
-	headers: {'X-RapidAPI-Host': 'tasty.p.rapidapi.com', 'X-RapidAPI-Key': 'a17c383771mshbf161638102ae4ep17ad69jsn001a69ad58e8'}
-};
+// function that displays the recipe info
+// function displayRecipe() {
+//     element.forEach(function(result, i) {
+//         if (i == 0) {
+//             //add start row
+//             content += '<div class="row">'
+//         }
+//         content += '<div class="col s12 m5">'
+//         content += '<div class="card-panel teal">'
+//         content += '<span class="white-text">recipe goes here, probably in an ordered list</span>'  
+//     })
 
-fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=5&tags=under_10_minutes&q=steak', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+//     content += '</div>';
+//     content += '</div>';
+// }
 
-    var savedFavorites = function(recipe){
-        console.log(recipe);
-        favorites.push(recipe);
-        localStorage.setItem("favorites", JSON.stringify(favorites));
+// var FavoriteBtnEl = document.querySelector('#favoritefood');
+// var favoriteTerm = document.querySelector('#listGroup');
+// console.log("linked")
+// const options2 = {
+// 	method: 'GET',
+// 	headers: {'X-RapidAPI-Host': 'tasty.p.rapidapi.com', 'X-RapidAPI-Key': 'a17c383771mshbf161638102ae4ep17ad69jsn001a69ad58e8'}
+// };
+
+// fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=5&tags=under_10_minutes&q=steak', options2)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+//     var savedFavorites = function(recipe){
+//         console.log(recipe);
+//         favorites.push(recipe);
+//         localStorage.setItem("favorites", JSON.stringify(favorites));
     
-        var displayfavorite = function(){
-        searchTerm.innerHTML = "";
-        for( let i =0; i< favorites.length; i++){
-            var list = document.createElement("li");
-            list.textContent = favorites[i];
+//         var displayfavorite = function(){
+//         searchTerm.innerHTML = "";
+//         for( let i =0; i< favorites.length; i++){
+//             var list = document.createElement("li");
+//             list.textContent = favorites[i];
 
-            list.addEventListener("click",function(event){
-                var favorites = event.target.textContent
-                getRecipte(recipe);   
-            })
-            searchTerm.append(list);
-        }  
-        }
+//             list.addEventListener("click",function(event){
+//                 var favorites = event.target.textContent
+//                 getRecipte(recipe);   
+//             })
+//             searchTerm.append(list);
+//         }  
+//         }
+//     }
+    
     // displayfavorite();
-
