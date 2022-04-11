7a3422d2c59155b6d474cb4a9654325ef8646b7e
jittel <nathan.c.ginn@gmail.com>
2022-04-11 11:35:29 -0700

    initial api call works

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("linked")
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+        'X-RapidAPI-Key': 'a17c383771mshbf161638102ae4ep17ad69jsn001a69ad58e8'
+    }
+};
+
+function getRecipe() {
+    console.log('tasty recipe function');
+
+    fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=5&tags=under_30_minutes', options)
+        .then(response => response.json())
+        .then(response => {
+            // console.log(response.results)
+            for (let i = 0; i < response.results.length; i++) {
+                const element = response.results[i];
+                console.log(element, element.video_url)
+
+            }
+        })
+        .catch(err => console.error(err));
+}
+
+getRecipe();
+
