7669d4c0e1859b02d8f17ed9606952cff0c44b61
jittel <nathan.c.ginn@gmail.com>
2022-04-13 12:43:20 -0700

    changed modals in html, finished showing instructions on modals

--- script.js
+++ script.js
@@ -79,36 +79,62 @@ function getRecipe() {
                 var recipeSpan = [];
                 
                 // makes list to click to show cards/modals w recipes
-                var content = `<h4 class="recipeName">${element.name}</h4>`;
+                var content = `<h4 class="recipeName modal-trigger" href="#modal${i}">${element.name}</h4>`;
                 
                 // shows the recipe names on the page
                 recipeContainer.innerHTML += content;
                 
                 recipeName = document.getElementsByClassName("recipeName");
-                
-                // recipeName[i].addEventListener("click", displayRecipe)
 
-                
-                // TODO: go through instructions and display them on cards/modals THIS ONLY HAPPENS ONCE CAUSE IT'S IN THE FOR LOOP DUMMY
+                document.getElementById(`recipe-title${i}`).textContent = element.name;
+
+                // TODO: go through instructions and display them on cards/modals
                 element.instructions.forEach(element => {
                     
                     // make it an ordered list
+                    var listItems = document.createElement("li");
+                    listItems.textContent = element.display_text
                     recipeSpan.push(`<li class="white-text">${element.display_text}</li>`)
-                    // console.log(element.display_text)
+                    console.log(listItems, "list items")
+                    document.getElementById(`instructions-list${i}`).appendChild(listItems)
                 })
 
                 // adds instructions to instructions list array
-                var instructionsList = recipeSpan.join("")
+                // var instructionsList = recipeSpan.join("")
                 // makes the card for the recipe to be sidplayed on
-                var recipeCard = `<div class="row"><div class="col s12 m5"><div class="card-panel teal"><ol>${instructionsList}</ol></div></div></div>`
-                console.log(recipeCard)
+                // var recipeCard = `<div id="modal1" class="modal modal-fixed-footer"><div class="modal-content"><ol>${instructionsList}</ol></div></div>`
+                console.log(recipeSpan)
+
+                //sets the name of recipe in modal
+                // $("h4").click(function(event) {
+                //     document.getElementById(`recipe-title${i}`).textContent = event.target.innerHTML;
+                // })
+
+                // set the instructions content in each modal
+                // document.getElementById(`instructions-list${i}`).appendChild(recipeSpan)
+                //  var stinky = document.getElementById(`instructions-list${i}`)
+                //  console.log(stinky, "stinky")
 
                 // makes any of the recipe names clickable
-                $("h4").click(function(event) {
-                    console.log(event.target.innerHTML)
+                // $("h4").click(function(event) {
+                //     console.log(event.target.innerHTML)
 
-                    // TODO: show the card of the item clicked on
-                })
+                //     // TODO: only showing the last one again :(
+                //     document.getElementById("recipe-title").textContent = event.target.innerHTML;
+                //     // document.getElementById("instructions-list").append(recipeSpan)
+                //     // recipeSpan.forEach((element, i) => {
+                //     //     document.getElementById(`instructions-list${i}`).append(element)
+                //     //     console.log("this is i, ", i)
+                //     // })
+                    
+                //     for (let i = 0; i < recipeSpan.length; i++) {
+                //         const element = recipeSpan[i];
+                        
+                //         document.getElementById(`instructions-list${i}`).append(element)
+                //     }
+
+                //     // TODO: show the card of the item clicked on
+                // })
             }
         })
         .catch(err => console.error(err));
@@ -183,4 +209,4 @@ function startTimer() {
       }
     }, 1000);
    }
-   
+}
